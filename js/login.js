70b6bc848ffeac6b9e62491351b13372c0bd51a4
Sebastian Sileoni <seba20sa@gmail.com>
2021-04-05 10:08:53 -0300

    I made some changes on the if else structure of the validation function to make sure it works as required

--- js/login.js
+++ js/login.js
@@ -4,6 +4,10 @@ var formLoginPassword = document.getElementById('form-login-password');
 var errorLoginContainer = document.getElementById('error-log-container');
 var formLoginButton = document.getElementById('login-button');
 var listOfErrors = document.getElementById('list-of-errors');
+var labelsCounter = document.getElementsByTagName('label')
+var inputsCounter = document.getElementsByTagName('input')
+var buttonsCounter = document.getElementsByTagName('button')
+var formCounter = document.getElementsByTagName('form')
 
 /*EVENT LISTENERS*/
 formLoginButton.addEventListener('click', submitLoginForm);
@@ -24,19 +28,48 @@ well*/
 function submitLoginForm(e) {    
     e.preventDefault();
     errorLoginContainer.classList.toggle('hidden');
-    if (formLoginEmail.value.length === 0) {
-        console.log('E-mail is invalid');        
-        listOfErrors.appendChild(createMenuItem('E-mail is invalid'));        
-    } else {
-        listOfErrors.appendChild(createMenuItem('E-mail is not empty'));
-        console.log('E-mail is not empty'); 
-    }
-    if (formLoginPassword.value.length === 0) {
-        listOfErrors.appendChild(createMenuItem('Password is invalid'));
-        console.log('Password is invalid');      
-        
-    } else {
-        listOfErrors.appendChild(createMenuItem('Password is not empty')); 
-        console.log('Password is not empty');
-    }
+    /*validation for elements*/
+    if (
+        formCounter && labelsCounter.length === 2 && inputsCounter.length === 2 &&
+         buttonsCounter.length === 2 && formLoginEmail.value.length !==0 && 
+         formLoginPassword.value.length !== 0 
+        ) {
+            listOfErrors.appendChild(createMenuItem('Everything is fine')); 
+        } else {
+            if (!formCounter) {
+                listOfErrors.appendChild(createMenuItem('There is no form in DOM')); 
+            }
+            if (labelsCounter.length === 0) {
+                listOfErrors.appendChild(createMenuItem('There are no labels in the form')); 
+            } else if (labelsCounter.length < 2) {
+                listOfErrors.appendChild(createMenuItem('There are missing labels in the form')); 
+            }  else if (labelsCounter.length >= 2) {
+                listOfErrors.appendChild(createMenuItem('There are ' + labelsCounter.length + ' labels in the form'));
+            }
+            if (inputsCounter.length === 0) {
+                listOfErrors.appendChild(createMenuItem('There are no inputs in the form')); 
+            } else if (inputsCounter.length < 2) {
+                listOfErrors.appendChild(createMenuItem('There are missing inputs in the form')); 
+            }  else if (inputsCounter.length >= 2) {
+                listOfErrors.appendChild(createMenuItem('There are ' + inputsCounter.length + ' inputs in the form'));
+            }
+            if (buttonsCounter.length === 0) {
+                listOfErrors.appendChild(createMenuItem('There are no buttons in the form')); 
+            } else if (buttonsCounter.length < 2) {
+                listOfErrors.appendChild(createMenuItem('There are missing buttons in the form')); 
+            }  else if (buttonsCounter.length >= 2) {
+                listOfErrors.appendChild(createMenuItem('There are ' + buttonsCounter.length + ' buttons in the form'));
+            }
+            if (formLoginEmail.value.length === 0) {
+              
+                listOfErrors.appendChild(createMenuItem('The e-mail field is empty'));        
+            } else {
+                listOfErrors.appendChild(createMenuItem('The e-mail field is not empty'));                
+            }
+            if (formLoginPassword.value.length === 0) {
+                listOfErrors.appendChild(createMenuItem('Password is invalid'));
+            } else {
+                listOfErrors.appendChild(createMenuItem('Password is not empty'));
+            }
+        }
 }
