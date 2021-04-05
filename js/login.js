e0cea1cb81f1dfd9d32b09a2ab829c5d36e72b8b
Sebastian Sileoni <seba20sa@gmail.com>
2021-04-05 10:14:49 -0300

    created a new class for error messages to show them in red

--- js/login.js
+++ js/login.js
@@ -37,37 +37,37 @@ function submitLoginForm(e) {
             listOfErrors.appendChild(createMenuItem('Everything is fine')); 
         } else {
             if (!formCounter) {
-                listOfErrors.appendChild(createMenuItem('There is no form in DOM')); 
+                listOfErrors.appendChild(createMenuItem('There is no form in DOM')).classList.toggle('error-message'); 
             }
             if (labelsCounter.length === 0) {
-                listOfErrors.appendChild(createMenuItem('There are no labels in the form')); 
+                listOfErrors.appendChild(createMenuItem('There are no labels in the form')).classList.toggle('error-message'); 
             } else if (labelsCounter.length < 2) {
-                listOfErrors.appendChild(createMenuItem('There are missing labels in the form')); 
+                listOfErrors.appendChild(createMenuItem('There are missing labels in the form')).classList.toggle('error-message'); 
             }  else if (labelsCounter.length >= 2) {
                 listOfErrors.appendChild(createMenuItem('There are ' + labelsCounter.length + ' labels in the form'));
             }
             if (inputsCounter.length === 0) {
-                listOfErrors.appendChild(createMenuItem('There are no inputs in the form')); 
+                listOfErrors.appendChild(createMenuItem('There are no inputs in the form')).classList.toggle('error-message'); 
             } else if (inputsCounter.length < 2) {
-                listOfErrors.appendChild(createMenuItem('There are missing inputs in the form')); 
+                listOfErrors.appendChild(createMenuItem('There are missing inputs in the form')).classList.toggle('error-message'); 
             }  else if (inputsCounter.length >= 2) {
                 listOfErrors.appendChild(createMenuItem('There are ' + inputsCounter.length + ' inputs in the form'));
             }
             if (buttonsCounter.length === 0) {
-                listOfErrors.appendChild(createMenuItem('There are no buttons in the form')); 
+                listOfErrors.appendChild(createMenuItem('There are no buttons in the form')).classList.toggle('error-message'); 
             } else if (buttonsCounter.length < 2) {
-                listOfErrors.appendChild(createMenuItem('There are missing buttons in the form')); 
+                listOfErrors.appendChild(createMenuItem('There are missing buttons in the form')).classList.toggle('error-message'); 
             }  else if (buttonsCounter.length >= 2) {
                 listOfErrors.appendChild(createMenuItem('There are ' + buttonsCounter.length + ' buttons in the form'));
             }
             if (formLoginEmail.value.length === 0) {
               
-                listOfErrors.appendChild(createMenuItem('The e-mail field is empty'));        
+                listOfErrors.appendChild(createMenuItem('The e-mail field is empty')).classList.toggle('error-message');        
             } else {
                 listOfErrors.appendChild(createMenuItem('The e-mail field is not empty'));                
             }
             if (formLoginPassword.value.length === 0) {
-                listOfErrors.appendChild(createMenuItem('Password is invalid'));
+                listOfErrors.appendChild(createMenuItem('Password is invalid')).classList.toggle('error-message');
             } else {
                 listOfErrors.appendChild(createMenuItem('Password is not empty'));
             }
